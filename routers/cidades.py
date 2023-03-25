4567965abd8cfadcc686d712ee37c42089195c63
SaraCoelhoCosta <costa.saracoelho@gmail.com>
2023-03-25 14:49:19 -0300

    Atualização métodos HTTP recebendo XML

--- routers/cidades.py
+++ routers/cidades.py
@@ -2,7 +2,9 @@ from sqlalchemy.orm import Session
 from database.models import Cidades
 from services.services import CidadesService
 from schemas.schemas import CidadesRequest, CidadesResponse
-from fastapi import APIRouter, Depends, HTTPException, status, Response
+from fastapi import APIRouter, Depends, HTTPException, status, Response, Request
+import xml.etree.ElementTree as ET
+import xmltodict
 from database.connection import get_db
 
 router = APIRouter()
@@ -25,19 +27,33 @@ def find_id(id: int, db: Session = Depends(get_db)):
 
 
 @router.post("", response_model=CidadesResponse, status_code=status.HTTP_201_CREATED)
-def create(request: CidadesRequest, db: Session = Depends(get_db)):
-    cidade = CidadesService.save(db, Cidades(**request.dict()))
-    return CidadesResponse.from_orm(cidade)
+async def create(request: Request, db: Session = Depends(get_db)):
+    content_type = request.headers.get("Content-Type")
+    if content_type == "application/json":
+        cidade = CidadesService.save(db, Cidades(**request.dict()))
+        return CidadesResponse.from_orm(cidade)
+    elif content_type == "application/xml":
+        xml = await request.body()
+        json = xmltodict.parse(xml)
+        cidade = CidadesService.save(db, Cidades(**json['cidades']))
+        return CidadesResponse.from_orm(cidade)
 
 
 @router.put("/{id}", response_model=CidadesResponse)
-def update(id: int, request: CidadesRequest, db: Session = Depends(get_db)):
+async def update(id: int, request: Request, db: Session = Depends(get_db)):
     if not CidadesService.exists_id(db, id):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Cidade não encontrada!"
         )
-    curso = CidadesService.save(db, Cidades(**request.dict()))
-    return CidadesResponse.from_orm(curso)
+    content_type = request.headers.get("Content-Type")
+    if content_type == "application/json":
+        cidade = CidadesService.save(db, Cidades(**request.dict()))
+        return CidadesResponse.from_orm(cidade)
+    elif content_type == "application/xml":
+        xml = await request.body()
+        json = xmltodict.parse(xml)
+        cidade = CidadesService.save(db, Cidades(**json['cidades']))
+        return CidadesResponse.from_orm(cidade)
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
