7601a9f34ab933b3da4b87c4c81fcf4093089677
Lara <larafabian061@gmail.com>
2023-05-03 22:35:20 -0300

    Atualização: Consumindo a API - Agencias

--- routers/agencias.py
+++ routers/agencias.py
@@ -10,54 +10,57 @@ from database.connection import get_db
 router = APIRouter()
 
 
-@router.get("", response_model=list[AgenciasResponse])
+@router.get("")
 def list(request: Request, db: Session = Depends(get_db)):
     agencias = AgenciasService.list(db)
-    response_obj = [AgenciasResponse.from_orm(agencia) for agencia in agencias]
-
-    root = ET.Element("agencias")
-    for agencia in response_obj:
-        agencia_elem = ET.SubElement(root, "agencia")
-        ET.SubElement(agencia_elem, "id").text = str(agencia.id)
-        ET.SubElement(agencia_elem, "descricao").text = agencia.descricao
-        ET.SubElement(agencia_elem, "sede").text = agencia.sede
-        ET.SubElement(agencia_elem, "cidades_id").text = str(agencia.cidades_id)
-        
-        xml_str = ET.tostring(root)
 
     # Verifica o formato da resposta
     accept = request.headers.get("Accept")
-    if accept == "application/json":
+    if accept == "application/json"or accept == "text/plain" or accept == "*/*" or accept == "application/json, text/plain, */*":
         return [AgenciasResponse.from_orm(agencia) for agencia in agencias]
-    elif accept == "application/xml":    
+    elif accept == "application/xml" or accept == "text/plain" or accept == "*/*" or accept == "application/xml, text/plain, */*":
+        response_obj = [AgenciasResponse.from_orm(agencia) for agencia in agencias]
+        
+        root = ET.Element("agencias")
+        for agencia in response_obj:
+            agencia_elem = ET.SubElement(root, "agencia")
+            ET.SubElement(agencia_elem, "id").text = str(agencia.id)
+            ET.SubElement(agencia_elem, "descricao").text = agencia.descricao
+            ET.SubElement(agencia_elem, "sede").text = agencia.sede
+            ET.SubElement(agencia_elem, "cidades_id").text = str(agencia.cidades_id)
+            
+        xml_str = ET.tostring(root)    
         return Response(content=xml_str, media_type="application/xml")
 
 
-@router.get("/{id}", response_model=AgenciasResponse)
+@router.get("/{id}")
 def find_id(request: Request, id: int, db: Session = Depends(get_db)):
+    
     agencia = AgenciasService.get_id(db, id)
     if not agencia:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Agência não encontrada!"
         )
-
-    root = ET.Element("agencia")
-    ET.SubElement(root, "id").text = str(agencia.id)
-    ET.SubElement(root, "descricao").text = agencia.descricao
-    ET.SubElement(root, "sede").text = agencia.sede
-    ET.SubElement(root, "cidades_id").text = str(agencia.cidades_id)
     
-    xml_str = ET.tostring(root)
-
     # Verifica o formato da resposta
     accept = request.headers.get("Accept")
-    if accept == "application/json":
+    if accept == "application/json" or accept == "text/plain" or accept == "*/*" or accept == "application/xml, text/plain, */*": 
         return AgenciasResponse.from_orm(agencia)
-    elif accept == "application/xml":    
+    
+    elif accept == "application/xml" or accept == "text/plain" or accept == "*/*" or accept == "application/xml, text/plain, */*":   
+        response_obj = AgenciasResponse.from_orm(agencia)
+
+        root = ET.Element("agencia")
+        ET.SubElement(root, "id").text = str(agencia.id)
+        ET.SubElement(root, "descricao").text = agencia.descricao
+        ET.SubElement(root, "sede").text = agencia.sede
+        ET.SubElement(root, "cidades_id").text = str(agencia.cidades_id)
+    
+        xml_str = ET.tostring(root)
         return Response(content=xml_str, media_type="application/xml")
 
 
-@router.post("", response_model=AgenciasResponse, status_code=status.HTTP_201_CREATED)
+@router.post("")
 async def create(request: Request, db: Session = Depends(get_db)):
     content_type = request.headers.get("Content-Type")
     accept = request.headers.get("Accept")
@@ -66,32 +69,48 @@ async def create(request: Request, db: Session = Depends(get_db)):
     if content_type == "application/json":
         json = await request.json()
         agencia = AgenciasService.save(db, Agencias(**json))
+
         # Verifica o formato da resposta
-        if accept == "application/json":
+        if accept == "application/json" or accept == "text/plain" or accept == "*/*" or accept == "application/json, text/plain, */*":
             return AgenciasResponse.from_orm(agencia)
-        elif accept == "application/xml":    
+        elif accept == "application/xml" or accept == "text/plain" or accept == "*/*" or accept == "application/json, text/plain, */*":    
+            xml = await request.body()
+            json = xmltodict.parse(xml)
+            agencia = AgenciasService.save(db, Agencias(**json['agencias']))
+            
+            root = ET.Element("agencia")
+            ET.SubElement(root, "id").text = str(agencia.id)
+            ET.SubElement(root, "descricao").text = agencia.descricao
+            ET.SubElement(root, "sede").text = agencia.sede
+            ET.SubElement(root, "cidades_id").text = str(agencia.cidades_id)
+            
+            xml_str = ET.tostring(root)
+            
             return Response(content=xml_str, media_type="application/xml")
+    
+    # Verifica formato da requisição
     elif content_type == "application/xml":
-        xml = await request.body()
-        json = xmltodict.parse(xml)
-        agencia = AgenciasService.save(db, Agencias(**json['agencias']))
-        
-        root = ET.Element("agencia")
-        ET.SubElement(root, "id").text = str(agencia.id)
-        ET.SubElement(root, "descricao").text = agencia.descricao
-        ET.SubElement(root, "sede").text = agencia.sede
-        ET.SubElement(root, "cidades_id").text = str(agencia.cidades_id)
-        
-        xml_str = ET.tostring(root)
-
+        print('Testando...')
         # Verifica o formato da resposta
-        if accept == "application/json":
+        if accept == "application/json" or accept == "text/plain" or accept == "*/*" or accept == "application/json, text/plain, */*":
             return AgenciasResponse.from_orm(agencia)
-        elif accept == "application/xml":    
+        
+        elif accept == "application/xml" or accept == "text/plain" or accept == "*/*" or accept == "application/xml, text/plain, */*": 
+            xml = await request.body()
+            json = xmltodict.parse(xml)
+            agencia = AgenciasService.save(db, Agencias(**json['agencias']))
+            
+            root = ET.Element("agencia")
+            ET.SubElement(root, "id").text = str(agencia.id)
+            ET.SubElement(root, "descricao").text = agencia.descricao
+            ET.SubElement(root, "sede").text = agencia.sede
+            ET.SubElement(root, "cidades_id").text = str(agencia.cidades_id)
+            
+            xml_str = ET.tostring(root)  
             return Response(content=xml_str, media_type="application/xml")
 
 
-@router.put("/{id}", response_model=AgenciasResponse)
+@router.put("/{id}")
 async def update(id: int, request: Request, db: Session = Depends(get_db)):
     if not AgenciasService.exists_id(db, id):
         raise HTTPException(
@@ -99,6 +118,8 @@ async def update(id: int, request: Request, db: Session = Depends(get_db)):
         )
     content_type = request.headers.get("Content-Type")
     accept = request.headers.get("Accept")
+    print(content_type)
+    print(accept)
     
     # Verifica o formato da requisição
     if content_type == "application/json":
@@ -114,19 +135,21 @@ async def update(id: int, request: Request, db: Session = Depends(get_db)):
         xml_str = ET.tostring(root)
 
         # Verifica o formato da resposta
-        if accept == "application/json":
+        if accept == "application/json" or accept == "text/plain" or accept == "*/*" or accept == "application/json, text/plain, */*":
             return AgenciasResponse.from_orm(agencia)
-        elif accept == "application/xml":    
+        elif accept == "application/xml" or accept == "text/plain" or accept == "*/*" or accept == "application/xml, text/plain, */*":    
             return Response(content=xml_str.decode("utf-8"), media_type="application/xml")
-        
+   
+    # Verifica o formato da requisição     
     elif content_type == "application/xml":
         xml = await request.body()
         json = xmltodict.parse(xml)
         agencia = AgenciasService.save(db, Agencias(**json['agencias']))
+
         # Verifica o formato da resposta
-        if accept == "application/json":
+        if accept == "application/json" or accept == "text/plain" or accept == "*/*" or accept == "application/json, text/plain, */*":
             return AgenciasResponse.from_orm(agencia)
-        elif accept == "application/xml":    
+        elif accept == "application/xml" or accept == "text/plain" or accept == "*/*" or accept == "application/json, text/plain, */*":    
             return Response(content=agencia.to_xml(), media_type="application/xml")
 
 
